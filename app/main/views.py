37f65f343594faf1e69c780e2806c1bc0007b4dd
Kvria <trishakuria@gmail.com>
2020-05-04 04:55:07 +0300

    method for profile update

---
+++ app/main/views.py
@@ -0,0 +1,26 @@
+from flask import render_template,request,redirect,url_for,abort
+from . import main
+from .forms import ReviewForm,UpdateProfile
+from ..models import User
+from .. import db,photos
+from flask_login import login_required, current_user
+import markdown2 
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
