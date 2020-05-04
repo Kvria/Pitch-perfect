a7dec1e229bd47d28c13e1c78750d4ca8eed5a92
Kvria <trishakuria@gmail.com>
2020-05-04 15:24:31 +0300

    add class for update profile

--- app/main/forms.py
+++ app/main/forms.py
@@ -3,3 +3,7 @@ from wtforms import StringField,TextAreaField,SubmitField
 from wtforms.validators import Required
 from flask_login import login_required
 from .. import db
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.',validators = [Required()])
+    submit = SubmitField('Submit')
