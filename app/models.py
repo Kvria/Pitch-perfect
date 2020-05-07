8456cda8de030b5fcddffdd53ae4ffcff466af06
Kvria <trishakuria@gmail.com>
2020-05-07 08:35:15 +0300

    installed flask login

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from . import db
 from datetime import datetime
+from werkzeug.security import generate_password_hash,check_password_hash
 
 # @login_manager.user_loader
 # def load_user(user_id):
@@ -17,20 +18,20 @@ class User(db.Model):
     password_secure = db.Column(db.String(255))
     posts = db.relationship('Post',backref = 'user', lazy = "dynamic")
 
-@property
-def password(self):
-    raise AttributeError('You cannot read the password attribute')
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
 
-@password.setter
-def password(self, password):
-    self.pass_secure = generate_password_hash(password)
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
 
 
-def verify_password(self,password):
-    return check_password_hash(self.pass_secure,password)
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
 
-def __repr__(self):
-    return f'User {self.username}'
+    def __repr__(self):
+        return f'User {self.username}'
 
 class Post(db.Model):
     __tablename__ = 'posts'
